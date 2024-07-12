0c8570016d6d62ce8d5c501e0d9d1578441a0d49
vangogh <van.vanbac010@gmail.com>
2024-07-12 11:19:26 +0700

    add some files & folder

---
+++ layouts/fields/com_tradingtech/loadpositions.php
@@ -0,0 +1,15 @@
+<?php
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Form\FormField;
+
+class JFormFieldLoadPositions extends FormField
+{
+    public function getInput()
+    {
+        return "<h1>hello world</h1>";
+    }
+}
+
+?>
