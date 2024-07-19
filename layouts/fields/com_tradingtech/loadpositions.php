c4b10164e8cd4097818e1b0dfeff9e14cdea6b54
vangogh <itmannavys@gmail.com>
2024-07-19 16:09:14 +0700

    + load more pages for trading position data + hide exclude position custom field

--- layouts/fields/com_tradingtech/loadpositions.php
+++ layouts/fields/com_tradingtech/loadpositions.php
@@ -50,20 +50,24 @@ class JFormFieldLoadPositions extends FormField
 		$pageLimit = vgTradingTechHelper::getParams('limit_positions');
         $totalPositions = round(vgComTradingTech::loadPositions(true) / $pageLimit);
         $html = '<ul class="vg-position-pagination">';
+        // $html .= '<li class="pag-prev" onclick="new vgApiHandling().loadPage(0, \'prev\', this)"><a href="#">&laquo;</a></li>';
         $html .= '<li class="pag-prev"><a href="#">&laquo;</a></li>';
 		for ($i=0; $i<$totalPositions; $i++)
 		{
             $page = $i + 1;
             $class = "page-$i";
             if ($i === 0) {
-                $class .= ' active';
+                $class .= ' active first';
+            } elseif ($i+1 == $totalPositions) {
+                $class .= ' last';
             }
-            if ($i < 3 || in_array($i, [$totalPositions-1, $totalPositions-2, $totalPositions-3])) {
-				$html .= "<li class='$class' onclick=' new vgApiHandling().loadPage($i, this)'><a>$page</a></li>";  // directly reload in page
+            if ($i < 5 || in_array($i, [$totalPositions-1, $totalPositions-2, $totalPositions-3, $totalPositions-4, $totalPositions-5])) {
+                $html .= "<li class='$class' onclick='new vgApiHandling().loadPage($i, \"currPage\", this)' curr-page='{$i}'><a>$page</a></li>";  // directly reload in page
             } elseif ($i == round($totalPositions/2)) {
-                $html .= "<li class='$class vg-position-pagination-dot' onclick='new vgApiHandling().loadPage($i, this)' class='vg-position-pagination-dot'><a>. . .</a></li>";
+                $html .= "<li class='$class vg-position-pagination-dot' onclick='new vgApiHandling().loadPage($i, \"currPage\", this)' curr-page='{$i}'><a>. . .</a></li>";
             }
         }
+        // $html .= '<li class="pag-next" onclick="new vgApiHandling().loadPage(0, \'next\', this)"><a href="#">&raquo;</a></li>';
         $html .= '<li class="pag-next"><a href="#">&raquo;</a></li>';
         $html .= '</ul>';
         return $html;
